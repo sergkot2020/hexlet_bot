c4bd200e25499371c38e2a0a0c87c310624df566
mjh-sakh <madjahead@gmail.com>
2021-07-18 06:58:33 +0300

    refactor to work without try, raise proper exception, use logging instead of prints

--- bot/reader.py
+++ bot/reader.py
@@ -1,38 +1,35 @@
 __all__ = [
-	'READ_CONFIG_DIRS',
-	'read_config'
+    'READ_CONFIG_DIRS',
+    'read_config'
 ]
 
-import os.path
+import logging
+import os
+from typing import Set, Dict
+
 import yaml
 
-READ_CONFIG_DIRS = ('.', './', '../')
+READ_CONFIG_DIRS = ('.', './', '../', 'bot')
 
 
-def read_config(filename, dirs=READ_CONFIG_DIRS):
-
-	if os.path.dirname(filename) == '':
-		f = None
-		ex = None
-
-		for d in dirs:
-			try:
-				f = open(os.path.join(d, filename))
-				break
-			except Exception as e:
-				ex = e
-
-		if f is None:
-			raise ex
-
-	else:
-		f = open(filename)
-
-	if hasattr(yaml, 'full_load'):
-		result = yaml.full_load(f)
-	else:
-		result = yaml.load(f)
-
-	f.close()
-
-	return result
+def read_config(filename: str, dirs: Set = READ_CONFIG_DIRS) -> Dict:
+    file_full_path: str
+    file_found_flag = False
+    if os.path.dirname(filename) == '':
+        logging.info(f'Started search of config file in {os.getcwd()}')
+        for dir in dirs:
+            file_full_path = os.path.join(dir, filename)
+            if os.path.exists(file_full_path):
+                file_found_flag = True
+                break
+    else:
+        file_full_path = filename
+    if not file_found_flag:
+        logging.error('Configuration file was not found')
+        raise FileNotFoundError()
+    with open(file_full_path) as file:
+        if hasattr(yaml, 'full_load'):
+            config = yaml.full_load(file)
+        else:
+            config = yaml.load(file)
+    return config
