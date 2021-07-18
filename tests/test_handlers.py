275c4b86550af6c090598f7a4ee83066505fe93a
mjh-sakh <madjahead@gmail.com>
2021-07-18 18:56:25 +0300

    change to at least working test

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,17 +1,20 @@
+import asyncio
+import logging
+
 from pytest import mark
-from telethon import TelegramClient
-from telethon.tl.custom.message import Message
+from telethon import TelegramClient, events
+
+from bot import DEV_CHANNEL_ID
+from bot.bot import message_handler
 
 
 @mark.asyncio
-async def test_help(bot_client: TelegramClient):
-    # Create a conversation
-    with bot_client.conversation("@myReplicaLikeBot", timeout=5) as conv:
-        # Send a command
-        await conv.send_message("/help")
-        # Get response
-        resp: Message = await conv.get_response()
-        # Make assertions
-        assert "@myReplicaLikeBot" in resp.raw_text
-        assert "👍" in resp.raw_text
-        assert "👎" in resp.raw_text
+async def test_help(bot_client: TelegramClient, caplog):
+    caplog.set_level(logging.DEBUG)
+    bot_client.on(events.NewMessage)(message_handler)
+    await bot_client.send_message(DEV_CHANNEL_ID, 'timer started')
+    await asyncio.sleep(5)
+    await bot_client.send_message(DEV_CHANNEL_ID, 'done')
+    # messages = await bot_client.get_messages(DEV_CHANNEL_ID)  # no permission
+    # logging.DEBUG(messages)
+
