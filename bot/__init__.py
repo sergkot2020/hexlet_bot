ad9b43eb0ef695c61286ae32421a1cbd3788ea50
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-07-25 13:23:56 +1100

    add helper class for configs

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,23 @@
 DEV_CHANNEL_ID = -1001422577723  # TODO: delete when not needed
+
+
+class ConfigHandler:
+    """
+    A convenience to have *entity.config_name* like access to configurations.
+
+    Example:
+        # create
+        bot_config = ConfigHandler(
+            name="bot",
+            key="key",
+        )
+        # use
+        start_bot(name=bot_config.name, key=bot_config.key)
+        or
+        start_bot(**bot_config.all())
+    """
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def all(self):
+        return self.__dict__
